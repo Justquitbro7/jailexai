a7ededc0122dab9baba8501a35b7ef47221dc9f9
Justquitbro7 <jeremysmoudy@gmail.com>
2026-03-10 14:08:07 -0700

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.justquitbro7.jailexai',
+  appName: 'Jailex AI',
+  webDir: 'app/src',
+  bundledWebRuntime: false
+};
+
+export default config;
