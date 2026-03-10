82d9fb602c3904953b11b0e2f6581d4192bf622b
Justquitbro7 <jeremysmoudy@gmail.com>
2026-03-10 11:16:28 -0700

    Create MainActivity.java

---
+++ app/src/app/src/main/java/com/jadenai/remote/MainActivity.java
@@ -0,0 +1,24 @@
+package com.jadenai.remote;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false); // Helps with audio/video in the HUD
+        
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
